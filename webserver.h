38ac3f2d75577c4435e7892caa0c17fa7ab19cc7
Janscode <55261974+Janscode@users.noreply.github.com>
2020-10-20 21:37:42 -0600

    Remove SERVER_ERROR string definition

--- webserver.h
+++ webserver.h
@@ -15,7 +15,6 @@
 
 #define MAX_REQUEST_QUEUE 128
 #define MAX_CONNECTION_QUEUE 128
-#define SERVER_ERROR "HTTP/1.1 500 Internal Server Error\nThese messages in the exact format as shown above should be sent back to the client if any of the above error occurs."
 
 
 void * serve_single_request(void * data);
