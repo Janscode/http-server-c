1fc93940a59ca5362b4576c6378cd8ca4bf979ba
Janscode <55261974+Janscode@users.noreply.github.com>
2020-10-20 22:55:06 -0600

    Removed commented out old code that I didn't need anymore

--- webserver.c
+++ webserver.c
@@ -1,64 +1,6 @@
 #include "webserver.h"
 
-
-
-//todo: add all needed header files
-
 int serving_requests = 1; //global variable for if the program should continue running
-
-// void end_thread(struct threadsafe_data * shared_data){
-//     /* safely decrement the thread count and then unsafely exit thread*/
-//     sem_wait(shared_data->thread_count_mutex);
-//         shared_data->thread_count--;
-//     sem_post(shared_data->thread_count_mutex);
-//     pthread_exit(NULL);
-// }
-
-// void start_thread(struct threadsafe_data * shared_data){
-//     /* safely increment the thread count, generate a new thread, and detach the thread */
-//     pthread_t tid;
-//     sem_wait(shared_data->thread_count_mutex);
-//         shared_data->thread_count++;
-//         pthread_create(&tid, NULL, &serve_single_request, (void *)shared_data);
-//         pthread_detach(tid);
-//     sem_post(shared_data->thread_count_mutex);
-    
-// }
-
-// int pop_connection(struct threadsafe_data * shared_data){ 
-//     int connection_fd;
-//     sem_wait(shared_data->queue_mutex);
-//         /* safely retreive a connection from the queue 
-//             warning: this function assumes that it is not called too frequently
-//         */
-//         //get socket file descriptor for the connection from the queue
-//         connection_fd = shared_data->connection_queue[shared_data->queue_start];
-
-//         //move index for start of queue
-//         shared_data->queue_start++;
-//         if (shared_data->queue_start == MAX_CONNECTION_QUEUE)
-//             shared_data->queue_start = 0;
-
-//     sem_post(shared_data->queue_mutex);
-//     return connection_fd;
-// }
-
-// void push_connection(int connection_fd, struct threadsafe_data * shared_data){
-//     /* safley add a connection to the queue
-//         warning: this function assumes the queue is not full
-//     */
-//     printf("Pushing connection \n");
-//     sem_wait(shared_data->queue_mutex);
-//         //add the socket file descriptor for the connection to the queue 
-//         shared_data->connection_queue[shared_data->queue_end] = connection_fd;
-
-//         //move index for end of queue
-//         shared_data->queue_end++;
-//         if (shared_data->queue_end == MAX_CONNECTION_QUEUE)
-//             shared_data->queue_end = 0;
-        
-//     sem_post(shared_data->queue_mutex);
-// }
 static char* error_string = "HTTP/1.1 500 Internal Server Error\r\n";
 static char* success_string = " 200 OK";
 static char* newline_string = "\r\n";
@@ -166,7 +108,7 @@ void * serve_single_request(void * connection_pointer){
         send(connection_fd, response_buffer, bytes_copied, 0);
     }
 
-   // send(connection_fd, error_string, strlen(error_string),0);
+    // send(connection_fd, error_string, strlen(error_string),0);
     fclose(fd);
     close(connection_fd);
     return(NULL);
@@ -187,18 +129,6 @@ int main(int argc, char ** argv){
     pthread_t tid;
     struct timeval timeout;
     struct sockaddr_in server_addr;
-    // struct threadsafe_data * shared_data;
-    
-    
-
-    // shared_data = malloc(sizeof(struct threadsafe_data));
-    
-    // shared_data->thread_count_mutex = sem_open("/thread_count_mutex", O_CREAT, 0777, 1);
-    // shared_data->queue_mutex = sem_open("/queue_mutex", O_CREAT, 0777, 1);
-
-    // shared_data->queue_end = 0;
-    // shared_data->queue_start = 0;
-    // shared_data->thread_count = 0;
 
     server_addr_len = sizeof(server_addr);
     
@@ -256,14 +186,9 @@ int main(int argc, char ** argv){
 
                 pthread_create(&tid, NULL, serve_single_request, (void *)connection_pointer);
                 pthread_detach(tid);
-                // push_connection(connection_fd, shared_data);
-                // printf("Added connection to list\n");
-                // start_thread(shared_data);
-                // printf("Started thread\n");
             }  
     }
     close(socket_fd);
-    //loop to check that all threads have terminated
     return(0);
 }
 
